4c813b89d6991c2c89c01fe5c63ddab849801ae5
Tachyonn <imgutti@gmail.com>
2018-02-14 18:53:15 +0200

    Update UserController.java. Added HTTP methods mappings.

--- src/main/java/com/tachyon/controller/UserController.java
+++ src/main/java/com/tachyon/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.tachyon.controller;
 
+import com.tachyon.domain.entity.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +11,21 @@ public class UserController {
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String getUser(@RequestParam int userId) {
-        return "<p>This is user section, GET method</p>";
+        return "<p>This is user section, GET method, by userId</p>";
     }
 
     @RequestMapping(value = "/user", method = RequestMethod.POST)
-    public String postUser() {
-        return "<p>This is user section, POST method</p>";
+    public String postUser(@RequestParam(value = "user", required = false) User user) { // TODO change to required=false
+        return "<p>This is user section, POST method</p>, creates new User";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.PUT)
+    public String putUser(@RequestParam(value = "user", required = false) User user) { // TODO change to required=false
+        return "<p>This is user section, PUT method</p>, update existing user  by User object.";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.DELETE)
+    public String deleteUser(@RequestParam(value = "userID", required = false) Long userID) { // TODO change to required=false
+        return "<p>This is user section, DELETE method</p>, delete existing user by userID.";
     }
 }
