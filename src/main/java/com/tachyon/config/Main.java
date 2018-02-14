0765a5be5ba435e1aff1cdb26b02410cca85744d
Tachyonn <imgutti@gmail.com>
2018-02-14 19:40:34 +0200

    Make minor annotation changes in UserController.java

--- src/main/java/com/tachyon/config/Main.java
+++ src/main/java/com/tachyon/config/Main.java
@@ -1,8 +1,6 @@
 package com.tachyon.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 public class Main{
     public static void main(String[] args) {
--- src/main/java/com/tachyon/controller/UserController.java
+++ src/main/java/com/tachyon/controller/UserController.java
@@ -1,31 +1,33 @@
 package com.tachyon.controller;
 
 import com.tachyon.domain.entity.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public String getUser(@RequestParam int userId) {
+    @GetMapping(path = "/user")
+    public @ResponseBody String getUser(@RequestParam int userId) {
         return "<p>This is user section, GET method, by userId</p>";
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.POST)
-    public String postUser(@RequestParam(value = "user", required = false) User user) { // TODO change to required=true
+    @PostMapping(path = "/user")
+    public @ResponseBody String postUser(@RequestParam(value = "user", required = false) User user) { // TODO change to required=true
         return "<p>This is user section, POST method</p>, creates new User";
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.PUT)
-    public String putUser(@RequestParam(value = "user", required = false) User user) { // TODO change to required=true
+    @PutMapping(path = "/user")
+    public @ResponseBody String putUser(@RequestParam(value = "user", required = false) User user) { // TODO change to required=true
         return "<p>This is user section, PUT method</p>, update existing user  by User object.";
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.DELETE)
-    public String deleteUser(@RequestParam(value = "userID", required = false) Long userID) { // TODO change to required=true
+    @DeleteMapping(path = "/user")
+    public @ResponseBody String deleteUser(@RequestParam(value = "userID", required = false) Long userID) { // TODO change to required=true
         return "<p>This is user section, DELETE method</p>, delete existing user by userID.";
+    }
+
+    @GetMapping(path = "/users")
+    public @ResponseBody String getAllUsers(){
+        return "<p>This is user section, get all users.";
     }
 }
