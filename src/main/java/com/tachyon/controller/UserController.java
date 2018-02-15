e8992fb2b9d26fb848cd28e5b5a653065348e441
Tachyonn <31632075+Tachyonn@users.noreply.github.com>
2018-02-15 10:20:38 +0200

    Minor fixes to annotations in UserController.java

--- src/main/java/com/tachyon/controller/UserController.java
+++ src/main/java/com/tachyon/controller/UserController.java
@@ -3,31 +3,31 @@ package com.tachyon.controller;
 import com.tachyon.domain.entity.User;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController // == @Controller + @ResponseBody
 public class UserController {
 
-    @GetMapping(path = "/user")
-    public @ResponseBody String getUser(@RequestParam int userId) {
-        return "<p>This is user section, GET method, by userId</p>";
+    @GetMapping(path = "/user") //== @RequestMapping(value="/user", method=GET)
+    public String getUser(@RequestParam int userId) {
+        return "<p>This is user section, GET method, by userId</p>"; // always @ResponseBody because of @RestController annotation
     }
 
     @PostMapping(path = "/user")
-    public @ResponseBody String postUser(@RequestParam(value = "user", required = false) User user) { // TODO change to required=true
+    public String postUser(@RequestParam(value = "user", required = false) User user) { // TODO change to required=true
         return "<p>This is user section, POST method</p>, creates new User";
     }
 
     @PutMapping(path = "/user")
-    public @ResponseBody String putUser(@RequestParam(value = "user", required = false) User user) { // TODO change to required=true
+    public String putUser(@RequestParam(value = "user", required = false) User user) { // TODO change to required=true
         return "<p>This is user section, PUT method</p>, update existing user  by User object.";
     }
 
     @DeleteMapping(path = "/user")
-    public @ResponseBody String deleteUser(@RequestParam(value = "userID", required = false) Long userID) { // TODO change to required=true
+    public String deleteUser(@RequestParam(value = "userID", required = false) Long userID) { // TODO change to required=true
         return "<p>This is user section, DELETE method</p>, delete existing user by userID.";
     }
 
     @GetMapping(path = "/users")
-    public @ResponseBody String getAllUsers(){
+    public String getAllUsers(){
         return "<p>This is user section, get all users.";
     }
 }
